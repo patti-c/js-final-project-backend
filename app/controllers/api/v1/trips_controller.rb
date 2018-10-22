0faa6a41a10f02de70f014a8a8dd4ee5e55a78ee
patti-c <patricia.carter@flatironschool.com>
2018-10-22 16:33:48 -0400

    Added trip creation

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,3 +1,17 @@
 class Api::V1::TripsController < ApplicationController
 
+  def create
+    render json: Trip.create(trip_params), status: 201
+  end
+
+  def destroy
+    render json: Trip.find(params[:id]).destroy
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:destination, :origin, :name, :user_id)
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,10 +6,23 @@ class Api::V1::UsersController < ApplicationController
     render json: @user, status: 200
   end
 
+  def index
+    @users = User.all
+    render json: @users, status: 200
+  end
+
+  def create
+    render json: User.create(user_params)
+  end
+
   private
 
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
   def find_user
-    @user = User.find(params[:email])
+    @user = User.find(params[:id])
   end
 
 end
