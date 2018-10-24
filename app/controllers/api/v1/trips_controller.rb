02ae21ae651ffee16e7d46994cf505712b490350
patti-c <patricia.carter@flatironschool.com>
2018-10-24 13:29:20 -0400

    Added CRUD to trips

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,17 +1,41 @@
 class Api::V1::TripsController < ApplicationController
 
+  before_action :find_trip, only: [:show, :edit, :update, :destroy]
+
+  def trips
+    @trips = Trip.all
+    render json: @trips, status: 200
+  end
+
   def create
     render json: Trip.create(trip_params), status: 201
   end
 
   def destroy
-    render json: Trip.find(params[:id]).destroy
+    render json: @trip.destroy
+  end
+
+  def edit
+
+  end
+
+  def update
+    @trip.update(trip_params)
+    render json: @trip, status: 202
+  end
+
+  def show
+    render json: @post, status: 200
   end
 
   private
 
   def trip_params
     params.require(:trip).permit(:destination, :origin, :name, :user_id)
+  end
+
+  def find_trip
+    @trip = Trip.find(params[:id])
   end
 
 end
