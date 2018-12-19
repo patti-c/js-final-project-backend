2c0ca1472547a32dae1cf218b29c30c4cb552a40
Alexandra Pick-Aluas <alex@Alexandras-MBP.home>
2018-12-19 13:42:36 -0500

    cors lockdown

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,12 @@ Bundler.require(*Rails.groups)
 
 module MetroRider
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'metrorider.herokuapp.com'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
